87e3d8cd42fd33b9ab09a1fa84d41ecd866f8a82
teammcarthur <chrism.23092@obhs.school.nz>
2025-07-28 12:28:32 +1200

    created python file

---
+++ working_with_lists.py
@@ -0,0 +1,16 @@
+'''
+    authpr: Chris
+    date: 28/07/25
+    version: 1
+    description: working with lists
+'''
+
+import random
+
+name = input("Enter your name: ")
+age = int( input("Enter your age: "))
+
+names = []
+
+for i in range(5):
+    print(1)
